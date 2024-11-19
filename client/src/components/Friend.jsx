35dca94a16f53b32ffcf304d509e24a26bbac6ef
Jummanbd <85425626+Jummanbd@users.noreply.github.com>
2024-11-19 13:16:28 +0600

    Update Friend.jsx

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:8080/users/${_id}/${friendId}`,
+      `https://full-stack-social-media-app-xhml.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
